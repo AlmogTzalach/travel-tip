96030dab672c69f1e9cd3da67818b68b8ff6d285
Anton Kandyba <antonkandyba@gmail.com>
2022-06-22 18:57:46 +0300

    added delete functionality

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,12 +1,17 @@
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
 
+export const mapController = {
+	renderLocsTable,
+}
+
 window.onload = onInit
 window.onAddMarker = onAddMarker
 window.onPanTo = onPanTo
 window.onGetLocs = onGetLocs
 window.onGetUserPos = onGetUserPos
 window.onSearchAddress = onSearchAddress
+window.onDeleteLoc = onDeleteLoc
 
 function onInit() {
 	mapService
@@ -26,25 +31,23 @@ function onSearchAddress(ev) {
 		const marker = mapService.addMarker(latLng, address)
 		locService.addLoc(marker)
 		onPanTo(latLng.lat, latLng.lng)
+		renderLocsTable()
 	})
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-	console.log('Getting Pos')
 	return new Promise((resolve, reject) => {
 		navigator.geolocation.getCurrentPosition(resolve, reject)
 	})
 }
 
 function onAddMarker() {
-	console.log('Adding a marker')
 	mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
 }
 
 function onGetLocs() {
 	locService.getLocs().then((locs) => {
-		console.log('Locations:', locs)
 		document.querySelector('.locs').innerText = JSON.stringify(locs)
 	})
 }
@@ -63,7 +66,6 @@ function onGetUserPos() {
 }
 
 function onPanTo(lat, lng) {
-	console.log('Panning the Map')
 	mapService.panTo(lat, lng)
 }
 
@@ -79,7 +81,7 @@ function renderLocsTable() {
 			strHTML += `<div class="loc-line">
                             <div>${loc.name}</div>
                             <button onclick="onPanTo(${loc.lat}, ${loc.lng})">Go</button>
-                            <button onclik="onDeleteLoc(${loc.id})">Delete</button>
+                            <button onclick="onDeleteLoc('${loc.id}')">Delete</button>
                         </div>`
 		})
 		document.querySelector('.loc-table').innerHTML = strHTML

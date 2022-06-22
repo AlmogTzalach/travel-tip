d52f37b36a0106a3dc492acc32d997f9495c01f5
Almog Tzalach <almogtza@gmail.com>
2022-06-22 19:01:20 +0300

    Added get user pos and CSS

--- js/app.controller.js
+++ js/app.controller.js
@@ -9,79 +9,77 @@ window.onGetUserPos = onGetUserPos
 window.onSearchAddress = onSearchAddress
 
 function onInit() {
-	mapService
-		.initMap()
-		.then(() => {
-			console.log('Map is ready')
-		})
-		.catch(() => console.log('Error: cannot init map'))
+  mapService
+    .initMap()
+    .then(() => {
+      console.log('Map is ready')
+    })
+    .catch(() => console.log('Error: cannot init map'))
 
-	renderLocsTable()
+  renderLocsTable()
 }
 
 function onSearchAddress(ev) {
-	ev.preventDefault()
-	const address = document.querySelector('input').value
-	locService.getSearchLoc(address).then((latLng) => {
-		const marker = mapService.addMarker(latLng, address)
-		locService.addLoc(marker)
-		onPanTo(latLng.lat, latLng.lng)
-	})
+  ev.preventDefault()
+  const address = document.querySelector('input').value
+  locService.getSearchLoc(address).then((latLng) => {
+    const marker = mapService.addMarker(latLng, address)
+    locService.addLoc(marker)
+    onPanTo(latLng.lat, latLng.lng)
+  })
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-	console.log('Getting Pos')
-	return new Promise((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(resolve, reject)
-	})
+  console.log('Getting Pos')
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
 }
 
 function onAddMarker() {
-	console.log('Adding a marker')
-	mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
+  console.log('Adding a marker')
+  mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
 }
 
 function onGetLocs() {
-	locService.getLocs().then((locs) => {
-		console.log('Locations:', locs)
-		document.querySelector('.locs').innerText = JSON.stringify(locs)
-	})
+  locService.getLocs().then((locs) => {
+    console.log('Locations:', locs)
+    document.querySelector('.locs').innerText = JSON.stringify(locs)
+  })
 }
 
 function onGetUserPos() {
-	getPosition()
-		.then((pos) => {
-			console.log('User position is:', pos.coords)
-			document.querySelector(
-				'.user-pos'
-			).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
-		})
-		.catch((err) => {
-			console.log('err!!!', err)
-		})
+  getPosition()
+    .then((pos) => {
+      console.log('User position is:', pos.coords)
+      onPanTo(pos.coords.latitude, pos.coords.longitude)
+    })
+    .catch((err) => {
+      console.log('err!!!', err)
+    })
 }
 
 function onPanTo(lat, lng) {
-	console.log('Panning the Map')
-	mapService.panTo(lat, lng)
+  console.log('Panning the Map')
+  mapService.panTo(lat, lng)
 }
 
 function onDeleteLoc(id) {
-	locService.deleteLoc(id)
-	renderLocsTable()
+  locService.deleteLoc(id)
+  renderLocsTable()
 }
 
 function renderLocsTable() {
-	locService.getLocs().then((locs) => {
-		let strHTML = ''
-		locs.forEach((loc) => {
-			strHTML += `<div class="loc-line">
+  locService.getLocs().then((locs) => {
+    let strHTML = ''
+    locs.forEach((loc) => {
+      strHTML += `<div class="loc-line">
                             <div>${loc.name}</div>
                             <button onclick="onPanTo(${loc.lat}, ${loc.lng})">Go</button>
                             <button onclik="onDeleteLoc(${loc.id})">Delete</button>
                         </div>`
-		})
-		document.querySelector('.loc-table').innerHTML = strHTML
-	})
+    })
+    document.querySelector('.loc-table').innerHTML = strHTML
+  })
 }

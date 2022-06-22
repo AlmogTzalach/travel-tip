e44ec784d53ba3804a4546fae2b15a55eac6a3ad
Almog Tzalach <almogtza@gmail.com>
2022-06-22 17:38:50 +0300

    Added search function

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -2,12 +2,24 @@ import { storageService } from './storage.service.js'
 
 export const locService = {
   getLocs,
+  getSearchLoc,
 }
 
 const locs = [
   { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
   { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
 ]
+
+function getSearchLoc(address) {
+  const API_KEY = 'AIzaSyBS1RGUGXqbyr9Ry8oWmOOd5nQMSP7sgBk'
+  // address = address.replaceAll(' ', '+')
+
+  return fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${API_KEY}`
+  )
+    .then((res) => res.json())
+    .then((res) => res.results[0].geometry.location)
+}
 
 function getLocs() {
   return new Promise((resolve, reject) => {
